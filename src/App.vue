1f78eab5abaf194e652457ca1771599f6ddfa87a
Julio Noguera <noguerajulio24@gmail.com>
2017-11-13 14:28:16 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>Music APP</h1>
+
+    <select v-model="selectedCountry" name="">
+      <option v-for="country in countries" v-bind:value="country.value">{{country.name}}</option>
+    </select>
+    <spinner v-show="loading"></spinner>
+    <ul>
+      <artist v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid"></artist>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import artist from './components/artist.vue'
+import spinner from './components/spinner.vue'
+import getArtists from './api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [ ],
+      countries: [
+        { name: 'Argentina', value: 'argentina'},
+        { name: 'Colombia', value: 'colombia'},
+        { name: 'España', value: 'spain'}
+      ],
+      selectedCountry: 'argentina',
+      loading: true
+    }
+  },
+  components: {
+    artist,
+    spinner
+  },
+  methods: {
+    refreshArtist(){
+      const self = this
+      this.loading = true
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.loading = false
+          self.artists = artists
+        })
+    }
+  },
+  mounted: function() {
+    this.refreshArtist();
+  },
+  watch: {
+    selectedCountry: function(){
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default{
+  appnane: 'Music',
+  apiKey: '7380d2504660bfcf32dcda0868442943',
+  secret: 'edc3f63b4ae01855c9ef13fdbe202e50',
+  registeredTo: 'noguerajulio24'
+}
